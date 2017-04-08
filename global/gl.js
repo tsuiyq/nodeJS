e59ccd0ecb2560484a748c4a2cd6ba61ccca8df5
tsuiyq <914266133@qq.com>
2017-04-08 10:09:58 +0800

    gl.js commit

--- global/gl.js
+++ global/gl.js
@@ -2,3 +2,30 @@
 console.log('current js file: ' + __filename);
 //current JavaScript dir:
 console.log('current js dir: ' + __dirname);
+process.name = 'Sample Nodejs';
+
+//process.argv 存储胃命令行参数：
+console.log('arguments: ' + JSON.stringify(process.argv));
+
+//process.cwd() 返回当前工作目录：
+console.log('cwd: ' + process.cwd());
+
+//切换当前工作目录：
+var d = '/private/tmp';
+if (process.platform == 'win32'){
+    //如果是Windows，切换到c:\windows\system32
+    d = 'c:\\Windows\\System32';
+}
+process.chdir(d);
+console.log('current work dir ' + process.cwd());
+
+//process.nextTick()将在下一轮事件循环中调用：
+process.nextTick(function(){
+    console.log('nextTick callback!');
+});
+console.log('nextTick was set!');
+
+//程序即将退出时的回调函数：
+process.on('exit',function (code){
+    console.log('about to exit with code: ' + code);
+});
