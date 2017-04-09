cec9bf83c990a1937ff34579a68dfee1f041dc34
tsuiyq <914266133@qq.com>
2017-04-09 12:19:36 +0800

    hmac

---
+++ crypto/hmac.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+const hmac = crypto.createHmac('sha256','secret-key');
+
+hmac.update('Hello, world!');
+hmac.update('Hello, nodejs!');
+
+var r = hmac.digest('hex');
+console.log('hmac: ' + r);
